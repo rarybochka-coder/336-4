d5f3b1870a1813531a6a040d185d89090c587162
Mocarow <mocarowmm@gmail.com>
2026-06-02 20:28:20 +0200

    Лаб 2 та Лаб3 файли

---
+++ PROGRAM/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            helpContextToolStripMenuItem = new ToolStripMenuItem();
+            tablesToolStripMenuItem = new ToolStripMenuItem();
+            coToolStripMenuItem = new ToolStripMenuItem();
+            ordersToolStripMenuItem = new ToolStripMenuItem();
+            itemsToolStripMenuItem = new ToolStripMenuItem();
+            partsToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, helpToolStripMenuItem, tablesToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(7, 2, 0, 2);
+            menuStrip1.Size = new Size(933, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(93, 22);
+            exitToolStripMenuItem.Text = "Exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { aboutToolStripMenuItem, helpContextToolStripMenuItem });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(144, 22);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // helpContextToolStripMenuItem
+            // 
+            helpContextToolStripMenuItem.Name = "helpContextToolStripMenuItem";
+            helpContextToolStripMenuItem.Size = new Size(144, 22);
+            helpContextToolStripMenuItem.Text = "Help Context";
+            // 
+            // tablesToolStripMenuItem
+            // 
+            tablesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { coToolStripMenuItem, ordersToolStripMenuItem, itemsToolStripMenuItem, partsToolStripMenuItem });
+            tablesToolStripMenuItem.Name = "tablesToolStripMenuItem";
+            tablesToolStripMenuItem.Size = new Size(51, 20);
+            tablesToolStripMenuItem.Text = "Tables";
+            // 
+            // coToolStripMenuItem
+            // 
+            coToolStripMenuItem.Name = "coToolStripMenuItem";
+            coToolStripMenuItem.Size = new Size(131, 22);
+            coToolStripMenuItem.Text = "Customers";
+            coToolStripMenuItem.Click += coToolStripMenuItem_Click;
+            // 
+            // ordersToolStripMenuItem
+            // 
+            ordersToolStripMenuItem.Name = "ordersToolStripMenuItem";
+            ordersToolStripMenuItem.Size = new Size(131, 22);
+            ordersToolStripMenuItem.Text = "Orders";
+            ordersToolStripMenuItem.Click += ordersToolStripMenuItem_Click;
+            // 
+            // itemsToolStripMenuItem
+            // 
+            itemsToolStripMenuItem.Name = "itemsToolStripMenuItem";
+            itemsToolStripMenuItem.Size = new Size(131, 22);
+            itemsToolStripMenuItem.Text = "Items";
+            itemsToolStripMenuItem.Click += itemsToolStripMenuItem_Click;
+            // 
+            // partsToolStripMenuItem
+            // 
+            partsToolStripMenuItem.Name = "partsToolStripMenuItem";
+            partsToolStripMenuItem.Size = new Size(131, 22);
+            partsToolStripMenuItem.Text = "Parts";
+            partsToolStripMenuItem.Click += partsToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(933, 519);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpContextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tablesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem coToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ordersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itemsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem partsToolStripMenuItem;
+    }
+}
+
---
+++ PROGRAM/Form1.cs
@@ -0,0 +1,81 @@
+﻿using Lab1.FormsLR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "Зроблено: Рибочка Ростислав Андрійович\nГрупа:336\nРік:2026",
+                "Про програму",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+
+                );
+        }
+
+        private void coToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormCustomers f = new FormCustomers();
+            f.Show();
+        }
+
+        private void ordersToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormOrders f = new FormOrders();
+            f.Show();
+        }
+
+        private void itemsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormItems f = new FormItems();
+            f.Show();
+        }
+
+        private void partsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormParts f = new FormParts();
+            f.ShowDialog();
+        }
+        private void OpenHelp(string page = "index.html")
+        {
+            string helpPath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory, "Help", page);
+            System.Diagnostics.Process.Start(
+                new System.Diagnostics.ProcessStartInfo(helpPath)
+                { UseShellExecute = true });
+        }
+
+        private void змістToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenHelp("index.html");
+        }
+
+        private void helpContextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenHelp("customers.html");
+        }
+
+
+    }
+}
